50266b036c6110c9b76adb5462b32d96b310d4fa
aziosmanoff <nine.aziosmanoff@gmail.com>
2016-05-03 11:51:59 +0200

    first commit

---
+++ exo1/t.py
@@ -0,0 +1,11 @@
+prenom = raw_input("Entrez votre prenom : ")
+print("bonjour ") + prenom
+
+
+# for i in range(12):
+# 	print 3**i
+# 	# calculer l'exposant jusqu'a 12
+
+# l = "*"
+# for i in range(1,8,1):
+# 	print l*i
---
+++ exo2/t.py
@@ -0,0 +1,5 @@
+a = input("Entrez un chiffre : ")
+b = input("Entrez un autre chiffre : ")
+
+print "La somme de a et b est = ", a+b 
+#on met un input au lieu dun raw_input sinon il calcule la somme des strings
